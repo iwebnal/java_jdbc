60db89ca3d9d2dafce391705f7d3525e37426bb3
iwebnal <iwebnal@gmail.com>
2023-01-09 12:45:11 +0300

    Loading properties settings

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
-import org.postgresql.Driver;
 import util.ConnectionManager;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        System.out.println("Hello world!");
-
-        Class<Driver> driverClass = Driver.class;
         try(Connection connection = ConnectionManager.open()) {
             System.out.println(connection.getTransactionIsolation());
         }
--- src/util/ConnectionManager.java
+++ src/util/ConnectionManager.java
@@ -13,9 +13,12 @@ public final class ConnectionManager {
      * password = "1234";
      * */
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "1234";
+//    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+//    private static final String USERNAME = "postgres";
+//    private static final String PASSWORD = "1234";
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
 
     static {
         // for old java version
@@ -32,7 +35,12 @@ public final class ConnectionManager {
 
     public static Connection open() {
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+//            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)
+            );
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
